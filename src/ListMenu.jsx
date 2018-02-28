ed3df83146d285f1dcecd7904a387b2f1cf6efe8
Lisa Gee <zhusufeng@gmail.com>
2018-02-27 16:52:30 -0800

    Create simple ListMenu

---
+++ src/ListMenu.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class ListMenu extends React.Component {
+  render () {
+    return (
+      <div>
+        <h3>Lists</h3>
+        <ul>
+          <li>Temp</li>
+        </ul>
+      </div>
+    );
+  }
+}
