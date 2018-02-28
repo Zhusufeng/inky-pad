bfa8042457d71ad5c8e4ad9c44f10e3987a5c4a1
Lisa Gee <zhusufeng@gmail.com>
2018-02-27 17:02:26 -0800

    Create some info in App state to pass to ListMenu

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,21 @@ export default class App extends React.Component {
   constructor (props) {
     super(props);
     this.state = {
-
+      lists: {
+        homework: [
+          'Read Tale of Two Cities',
+          'Finish Chapter 2 problems'
+        ]
+      },
+      history: []
     };
   }
 
   render () {
     return (
-      <div> App rendered!
+      <div>
+        <h1>Inky Pad</h1>
+        <ListMenu lists={this.state.lists}></ListMenu>
       </div>
     );
   }
