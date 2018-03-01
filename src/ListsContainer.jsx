dd28cb507985239663e9d3d05052be01a230ea7c
Lisa Gee <zhusufeng@gmail.com>
2018-02-28 23:29:11 -0800

    Import List to ListsContainer & pass down individual list data

--- src/ListsContainer.jsx
+++ src/ListsContainer.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import List from './List.jsx';
 
 const ListsContainer = ({lists}) => (
   <div>
     {
       Object.keys(lists).map(list => {
-        return (<List />);
+        return (<List list={list}/>);
       })
     }
   </div>
