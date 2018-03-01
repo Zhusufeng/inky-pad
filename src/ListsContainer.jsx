690f73420dd9d3e6181a97304059eddcea96c04c
Lisa Gee <zhusufeng@gmail.com>
2018-02-28 23:27:43 -0800

    Create implicit return in ListsContainer

--- src/ListsContainer.jsx
+++ src/ListsContainer.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const ListsContainer = ({lists}) => {
-  return (
-    <div>
-      {
-        Object.keys(lists).map(list => {
-          return (<List />);
-        })
-      }
-    </div>
-  );
-};
+const ListsContainer = ({lists}) => (
+  <div>
+    {
+      Object.keys(lists).map(list => {
+        return (<List />);
+      })
+    }
+  </div>
+);
 
 export default ListsContainer;
