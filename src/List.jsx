b1ed20f3db8d54d183a4c8b347b3d257568e911c
Lisa Gee <zhusufeng@gmail.com>
2018-02-28 23:40:55 -0800

    Design out List component more with name & items

--- src/List.jsx
+++ src/List.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const List = ({list}) => (
-  <div>{list}</div>
+const List = ({name, items}) => (
+  <div>
+    <h3>{name}</h3>
+    <ul>
+      {items.map(item => {
+        return (<li>{item}</li>);
+      })}
+    </ul>
+    <form>
+      <input type="text" />
+      <input type="submit" />
+    </form>
+  </div>
 );
 
 
