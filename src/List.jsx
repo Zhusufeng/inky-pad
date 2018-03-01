32e0fe2c0584e20d0d67a0357ce9fc51ef0405e4
Lisa Gee <zhusufeng@gmail.com>
2018-03-01 00:01:10 -0800

    Cannot update state from child List

--- src/List.jsx
+++ src/List.jsx
@@ -1,20 +1,66 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const List = ({name, items}) => (
-  <div>
-    <h3>{name}</h3>
-    <ul>
-      {items.map(item => {
-        return (<li>{item}</li>);
-      })}
-    </ul>
-    <form>
-      <input type="text" />
-      <input type="submit" />
-    </form>
-  </div>
-);
+// const List = ({name, items}) => (
+//   <div>
+//     <h3>{name}</h3>
+//     <ul>
+//       {items.map(item => {
+//         return (<li>{item}</li>);
+//       })}
+//     </ul>
+//     <form>
+//       <input type="text" />
+//       <input type="submit" />
+//     </form>
+//   </div>
+// );
 
 
-export default List;
+// export default List;
+
+export default class List extends React.Component {
+  constructor (props) {
+    super(props);
+  }
+
+  addToList (e) {
+    e.preventDefault();
+    console.log('e is ', e);
+    console.log('e.target is ', e.target);
+    console.log('e.target[0].value is ', e.target[0].value);
+
+    // const newList = {[listName]: []};
+    // this.setState({
+    //   lists: Object.assign({}, this.state.lists, newList) 
+    // }, () => {
+    //   console.log(this.state);
+    // });
+
+    this.setState({
+      lists: {
+        potatoes: [],
+        beans: []
+      }
+    }, () => {
+      console.log(this.state);
+    })
+  }
+
+  render () {
+    return (
+      <div>
+        <h3>{this.props.name}</h3>
+        <ul> 
+          {this.props.items.map(item => {
+            return (<li>{item}</li>);
+          })}
+        </ul>
+        <form onSubmit={this.addToList.bind(this)}>
+          <input type="text" />
+          <input type="submit" />
+        </form>
+      </div>
+    );
+  }
+}
