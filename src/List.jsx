c1bd539af8baa7a1ec7715a671da932af3806d79
Lisa Gee <zhusufeng@gmail.com>
2018-03-22 15:08:19 -0700

    Remove comments from List

--- src/List.jsx
+++ src/List.jsx
@@ -1,27 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// const List = ({name, items}) => (
-//   <div>
-//     <h3>{name}</h3>
-//     <ul>
-//       {items.map(item => {
-//         return (<li>{item}</li>);
-//       })}
-//     </ul>
-//     <form>
-//       <input type="text" />
-//       <input type="submit" />
-//     </form>
-//   </div>
-// );
-
-
-// export default List;
-
 export default class List extends React.Component {
   constructor (props) {
     super(props);
+
   }
 
   addToList (e) {
@@ -29,32 +12,19 @@ export default class List extends React.Component {
     console.log('e is ', e);
     console.log('e.target is ', e.target);
     console.log('e.target[0].value is ', e.target[0].value);
-
-    // const newList = {[listName]: []};
-    // this.setState({
-    //   lists: Object.assign({}, this.state.lists, newList) 
-    // }, () => {
-    //   console.log(this.state);
-    // });
-
-    this.setState({
-      lists: {
-        potatoes: [],
-        beans: []
-      }
-    }, () => {
-      console.log(this.state);
-    })
   }
 
   render () {
+    console.log('this.props: ', this.props);
     return (
       <div>
         <h3>{this.props.name}</h3>
         <ul> 
-          {this.props.items.map((item, key) => {
-            return (<li key={key}>{item}</li>);
-          })}
+          {
+            this.props.items.map((item, key) => {
+              return (<li key={key}>{item}</li>);
+            })
+          }
         </ul>
         <form onSubmit={this.addToList.bind(this)}>
           <input type="text" />
