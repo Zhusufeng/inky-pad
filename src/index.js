bc5c6fdf33d555efe2893350d7fc5daa8d5b7b80
Lisa Gee <zhusufeng@gmail.com>
2018-03-22 15:13:28 -0700

    Update path of App in src/index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App.jsx';
+import App from './components/App.jsx';
 
 ReactDOM.render(
   <App />, 
-  document.getElementById('app')
+  document.querySelector('#app')
 );
 
 console.log('index.js rendered');
