63dde2c04dc2760e279238ad83690ebf7bd05db1
Lisa Gee <zhusufeng@gmail.com>
2018-03-22 14:53:49 -0700

    Add key to each li in ListMenu

--- src/ListMenu.jsx
+++ src/ListMenu.jsx
@@ -6,8 +6,8 @@ const ListMenu = ({lists, createList}) => (
     <h3>Lists</h3>
     <ul>
       {
-        Object.keys(lists).map(item => {
-          return (<li>{item}</li>);
+        Object.keys(lists).map((item, key) => {
+          return (<li key={key}>{item}</li>);
         })
       }
     </ul>
