f3c12bd5d38c296cab18f77c93ccef4960699f3f
Lisa Gee <zhusufeng@gmail.com>
2018-03-27 01:36:59 -0700

    Use Redux in ListsContainer to pass info to List components

--- src/components/ListsContainer.jsx
+++ src/components/ListsContainer.jsx
@@ -1,21 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
 import List from './List.jsx';
 
-const ListsContainer = ({lists}) => (
-  <div>
-    {
-      Object.keys(lists).map((list, key) => {
-        return (
-          <List 
-            name={list} 
-            items={lists[list]}
-            key={key}
-          />
-        );
-      })
-    }
-  </div>
-);
+class ListsContainer extends Component {
+  constructor (props) {
+    super(props);
+  }
 
-export default ListsContainer;
+  render () {
+    return (
+      <div>
+        {
+          Object.keys(this.props.lists).map((listName, key) => {
+            return (
+              <List 
+                name={listName} 
+                items={this.props.lists[listName]}
+                key={key}
+              />
+            );
+          })
+        }
+      </div>
+    );
+  }
+}
+
+function mapStateToProps (state) {
+  const { lists } = state; // grab lists object from store
+  return { lists };
+}
+
+export default connect(mapStateToProps, null)(ListsContainer);
