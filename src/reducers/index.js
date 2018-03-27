9bcc4368f5ca3430167af98dffc6d5d2c57d0203
Lisa Gee <zhusufeng@gmail.com>
2018-03-27 01:23:07 -0700

    Adjust reducer to add listName to state.lists as empty array

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,11 +4,38 @@ const rootReducer = (state = {}, action) => {
   switch(action.type) {
     case ADD_LIST:
       const { listName } = action; 
-      const newState = Object.assign(state, listName);
+      const lists = { 
+        [listName]: [] 
+      };
+      const newState = Object.assign({}, state, { lists });      
+      console.log('newState: ', newState);
       return newState; 
+
     default:
       return state;
   }
 };
 
 export default rootReducer;
+
+/* Tests
+
+// No lists inside
+const state = {};
+const action = {listName: 'groceries'};
+const { listName } = action; 
+const lists = { [listName]: [] };
+const newState = Object.assign({}, state, { lists });
+console.log(newState);
+
+// Has lists
+const state = { lists: {} };
+const action = {listName: 'groceries'};
+const { listName } = action; 
+const lists = { [listName]: [] };
+const newState = Object.assign({}, state, { lists });
+console.log(newState);
+
+*/
+
+
