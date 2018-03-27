a2332444e05d61269d898d115c5b4fc388bf6236
Lisa Gee <zhusufeng@gmail.com>
2018-03-26 23:25:44 -0700

    Simplify App since Redux has been implemented

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,57 +1,19 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { addList } from '../actions';
-
+import React, { Component } from 'react';
 import ListMenu from './ListMenu.jsx';
 import ListsContainer from './ListsContainer.jsx';
 
-class App extends React.Component {
+class App extends Component {
   constructor (props) {
     super(props);
-    this.state = {
-      lists: {
-        homework: [
-          'Read Tale of Two Cities',
-          'Finish Chapter 2 problems'
-        ],
-        chores: [
-          'Wash dishes',
-          'Do laundry'
-        ]
-      },
-      history: []
-    };
-  }
-
-  createList (listName) {
-    console.log('Create list was clicked: ', listName);
-    const newList = {[listName]: []};
-    console.log(newList);
-    this.setState({
-      lists: Object.assign({}, this.state.lists, newList) 
-    }, () => {
-      console.log(this.state);
-    });
   }
 
   render () {
     return (
       <div>
         <h1>Inky Pad</h1>
-        <ListMenu 
-          lists={this.state.lists}
-          createList={this.createList.bind(this)}
-        />
-        <ListsContainer 
-          lists={this.state.lists}
-        />
+        <ListMenu />
+        <ListsContainer />
       </div>
     );
   }
 }
-
-function mapStateToProps (state) {
-  return state;
-}
-
-export default connect(mapStateToProps, { addList })(App);
