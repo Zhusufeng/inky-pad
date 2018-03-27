6ef691635171d710b87bb5d49e12aeda58183256
Lisa Gee <zhusufeng@gmail.com>
2018-03-26 23:43:23 -0700

    Begin restructure of ListMenu with Redux

--- src/components/ListMenu.jsx
+++ src/components/ListMenu.jsx
@@ -1,33 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { addList } from '../actions';
 
-const ListMenu = ({lists, createList}) => (
-  <div>
-    <h3>Lists</h3>
-    <ul>
-      {
-        Object.keys(lists).map((item, key) => {
-          return (<li key={key}>{item}</li>);
-        })
-      }
-    </ul>
-    <input 
-      type="text" 
-      id="list-menu-input"
-      minLength="1"
-      maxLength="20" 
-      required
-    />
-    <button
-      onClick={(e) => {
-        const input = document.getElementById('list-menu-input').value;
-        return createList(input);
-      }}
-    >
-      Create List
-    </button>
-  </div>
+class ListMenu extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      newList: ''
+    };
+  }
 
-);
+  addList () {
 
-export default ListMenu;
+  }
+
+  render () {
+    return (
+      <div>
+        <h3>Lists</h3>
+        <ul>
+          {
+            Object.keys(this.props.lists).map((listName, key) => {
+              return (<li key={key}>{listName}</li>);
+            })
+          }
+        </ul>
+        <input 
+          type="text" 
+          id="list-menu-input"
+          required
+          onChange={(e) => this.setState({newList: e.target.value})}
+        />
+        <button
+          onClick={() => this.addList(input)}
+        >
+          Create List
+        </button>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return state;
+}
+
+export default connect(mapStateToProps, { addList })(ListMenu);
