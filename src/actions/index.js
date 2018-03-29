cf3535e2903a86b15bc7016f73a31cdc440ab393
Lisa Gee <zhusufeng@gmail.com>
2018-03-28 23:54:26 -0700

    Add new action creator addTask

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,4 @@
-import { ADD_LIST } from '../constants';
+import { ADD_LIST, ADD_TASK } from '../constants';
 
 export const addList = (listName) => {
   const action = {
@@ -6,5 +6,15 @@ export const addList = (listName) => {
     listName
   };
   console.log('action in addList', action);
+  return action;
+};
+
+export const addTask = (task, listName) => {
+  const action = {
+    type: ADD_TASK,
+    task,
+    listName
+  };
+  console.log('action in addTask', action);
   return action;
 };
