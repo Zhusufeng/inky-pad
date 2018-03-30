75040aed8c82326de417b5a2fdbb15156bd0fccc
Lisa Gee <zhusufeng@gmail.com>
2018-03-30 00:07:32 -0700

    Add FINISH_TASK to reducer switch statement

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,4 @@
-import { ADD_LIST, ADD_TASK } from '../constants';
+import { ADD_LIST, ADD_TASK, FINISH_TASK } from '../constants';
 
 const rootReducer = (state = {}, action) => {
   switch(action.type) {
@@ -21,6 +21,20 @@ const rootReducer = (state = {}, action) => {
       const newArray = [...state.lists[listName], task];
       const updateList = { 
         [listName]: newArray 
+      };
+      const lists = Object.assign({}, state.lists, updateList);
+      const newState = Object.assign({}, state, { lists });
+      console.log('newState: ', newState);
+      return newState;
+    }
+    break;
+
+    case FINISH_TASK: {
+      const { task, listName } = action;
+      const taskIndex = state.lists[listName].indexOf(task);
+      const newArray = state.lists[listName].slice(0, taskIndex).concat(state.lists[listName].slice(taskIndex));
+      const updateList = {
+        [listName]: newArray
       };
       const lists = Object.assign({}, state.lists, updateList);
       const newState = Object.assign({}, state, { lists });
