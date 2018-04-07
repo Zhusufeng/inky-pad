7d7f0cb1443b6ab24fcba514c328d57b46b908e2
Lisa Gee <zhusufeng@gmail.com>
2018-04-07 00:23:19 -0700

    Import styles.css to App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ListMenu from './ListMenu.jsx';
 import ListsContainer from './ListsContainer.jsx';
+import '../styles.css';
 
 class App extends Component {
   constructor (props) {
